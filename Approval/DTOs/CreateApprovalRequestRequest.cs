54e8a170b28844e07e9ff8e53cac91653e35ad3b
ChandiraVihan <chandiravihan@gmail.com>
2026-09-19 15:47:50 +0530

    feat: validated pending requests

--- Approval/DTOs/CreateApprovalRequestRequest.cs
+++ Approval/DTOs/CreateApprovalRequestRequest.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Approval.Service.DTOs;
 
 public sealed class CreateApprovalRequestRequest
 {
+    [Required]
+    [StringLength(100)]
     public string ResourceId { get; set; } = string.Empty;
+
+    [Range(1, 5)]
     public int RequestedLevel { get; set; }
+
+    [Required]
+    [StringLength(500)]
     public string Reason { get; set; } = string.Empty;
+
+    [Range(1, 1440)]
     public int DurationMinutes { get; set; }
 }
